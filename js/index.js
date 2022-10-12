84209391a59f0823fdb29202a51b4acf51632bbc
Hugo Violas <violas.hugo@gmail.com>
2022-10-12 15:58:29 +0200

    conditionnal part added

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
 // Iteration 1: Names and Input
 let hacker1 = "Vishnu";
 console.log(`The driver\'s name is ${hacker1}.`);
-// Iteration 2: Conditionals
-
-// Iteration 3: Loops
 let hacker2 = "Hugo";
 console.log(`The navigator\'s name is ${hacker2}.`);
+// Iteration 2: Conditionals
+if (hacker1.length > hacker2.length) {
+  console.log(
+    `The driver has the longest name, it has ${hacker1.length} characters.`
+  );
+} else if (hacker1.length < hacker2.length) {
+  console.log(
+    `It seems that the navigator has the longest name, it has ${hacker2.length} characters.`
+  );
+} else {
+  console.log(
+    `Wow, you both have equally long names, ${hacker1.length} characters!`
+  );
+}
+// Iteration 3: Loops
